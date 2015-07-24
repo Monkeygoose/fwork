7de6b6a8925d8e811acc1c1f7f4c31a621de3a14
Monkeygoose <chris@monkeygoosewebdesign.co.uk>
2015-07-24 10:00:57 +0100

    0.0.1c
    Article Dev

---
+++ public_html/application/logs/log-2015-07-15.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-07-15 17:31:34 --> 404 Page Not Found: Article/create_article
+ERROR - 2015-07-15 17:37:23 --> Severity: Notice --> Undefined variable: info C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\home.php 1
+ERROR - 2015-07-15 18:47:12 --> Severity: Notice --> Undefined variable: query C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:47:12 --> Severity: Error --> Call to a member function result() on a non-object C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:47:38 --> Severity: Notice --> Undefined variable: query C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:47:38 --> Severity: Error --> Call to a member function result() on a non-object C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:48:03 --> Severity: Notice --> Undefined variable: query C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:48:03 --> Severity: Error --> Call to a member function result() on a non-object C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:50:28 --> Severity: Notice --> Undefined variable: query C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 7
+ERROR - 2015-07-15 18:51:06 --> Severity: Parsing Error --> syntax error, unexpected T_STRING, expecting ',' or ';' C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 5
+ERROR - 2015-07-15 18:51:27 --> Severity: Notice --> Undefined variable: query C:\Users\Chris\Projects\www\fwork\public_html\application\views\admin\magazine\create_article.php 5
--- public_html/application/views/admin/magazine/create_article.php
+++ public_html/application/views/admin/magazine/create_article.php
@@ -1,3 +1,33 @@
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('article/create_article') ?>
+<?php echo form_open('article/create_article'); ?>
+
+	<input type="text" name="title" placeholder="Title"><br />
+
+	<input type="text" name="author" placeholder="Author"><br />
+
+	<select name="category">
+		<option disabled selected>Category</option>
+		<option value="art">Art</option>
+		<option value="music">Music</option>
+		<option value="poetry">Poetry</option>
+		<option value="design">Design</option>
+
+	</select><br />
+
+	<select name="issue_num">
+		<option disabled selected>Issue</option>
+		<?php 
+		foreach ($query->result() as $row) : 
+		?>
+			<option value="<?php echo $row->issue_num;?>"><?php echo $row->issue_num; ?></option>
+		<?php 
+		endforeach; 
+		?>
+	</select><br />
+
+	<textarea>Details</textarea><br />
+
+	<input type="submit" name="submit" value="Create Article" />
+
+</form>
